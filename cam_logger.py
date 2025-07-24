fae4f106e64eec8a9eb95e9054f1b9da6751409f
Ozan Gokberk <ozan8@hotmail.co.uk>
2025-07-24 22:43:43 +0100

    testing webcam feedback

---
+++ cam_logger.py
@@ -0,0 +1,14 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    if ret == True:
+        cv2.imshow("GhostTap Feed", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
